e59c256079531745c7f96eb43df8160b8741ec5f
Adzi <belkoadam@gmail.com>
2023-04-10 07:00:56 +1000

    file structure setup

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./Header";
+import Gameboard from "./Gameboard";
+
+const App = () => {
+    return (
+        <>
+            <Header />
+            <Gameboard />
+        </>
+    );
+};
+
+export default App;
---
+++ src/components/Gameboard.jsx
@@ -0,0 +1,9 @@
+const Game = () => {
+    return (
+        <>
+            <div>content</div>
+        </>
+    );
+};
+
+export default Game;
