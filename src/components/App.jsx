18456b3d5f11af98a82a4609a8191cac496281b1
Adzi <belkoadam@gmail.com>
2023-04-11 20:12:02 +1000

    minor update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Header from "./Header";
 import Gameboard from "./Gameboard";
 import cardList from "./utils/CardList";
